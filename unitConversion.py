a45d48dcadb85a5c7ff147bf1b3252881def5f3e
ficolas2 <nicolasdepg@gmail.com>
2018-01-21 22:10:01 +0100

    Create unitConversion.py

---
+++ unitConversion.py
@@ -0,0 +1,57 @@
+from abc import ABCMeta, abstractmethod
+from enum import Enum
+import re
+
+END_NUMBER_REGEX = re.compile("[0-9]+([\,\.][0-9]+)?\s*$")
+DECIMALS = 3
+
+class UnitTypes( Enum ):
+	DISTANCE = "m"
+	AREA="m^2"
+	VOLUME="L"
+	ENERGY="J"
+	FORCE="N"
+	TORQUE="N*m"
+	VELOCITY="m/s"
+	MASS="g"
+	TEMPERATURE="C"
+	PRESSURE="atm"
+
+class Unit:
+	def __init__( self, toSI, unitType ):
+		self._unitType = unitType
+		self._toSI = toSI
+	
+	def toSI( self, number ):
+		return str(round(number * self._toSI, DECIMALS)) + " " + self._unitType.value
+	
+	@abstractmethod
+	def convert( self, message ): pass
+
+
+class NormalUnit( Unit ):
+	def __init__( self, regex, toSI, unitType ):
+		super( NormalUnit, self ).__init__(toSI, unitType);
+		self._regex = re.compile( regex + "(?![a-z])")
+	
+	def convert( self, message ):
+		iterator = self._regex.finditer( message["text"] )
+		replacements = []
+		for find in iterator:
+			numberResult = END_NUMBER_REGEX.search( message[ "text" ][ 0 : find.start() ] )
+			if numberResult is not None:
+				repl = {}
+				repl[ "start" ] = numberResult.start()
+				repl[ "text"  ] = self.toSI( float( numberResult.group() ) ) 
+				repl[ "end" ] = find.end()
+				replacements.append(repl)
+		if len(replacements)>0:
+			message["modified"] = True
+			lastPoint = 0
+			finalMessage = ""
+			for repl in replacements:
+				finalMessage += message[ "text" ][ lastPoint: repl[ "start" ] ] + repl[ "text" ]
+				lastPoint = repl["end"]
+			finalMessage += message[ "text" ][ lastPoint : ]
+			message["text"] = finalMessage
+		return message
