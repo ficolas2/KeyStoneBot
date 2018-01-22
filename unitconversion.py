df4c5fb21bacb69c842c9d17ba0691d52e34f78b
ficolas2 <nicolasdepg@gmail.com>
2018-01-22 20:26:51 +0100

    Update unitconversion.py

--- unitconversion.py
+++ unitconversion.py
@@ -5,23 +5,27 @@ from enum import Enum
 import re
 
 END_NUMBER_REGEX = re.compile("[0-9]+([\,\.][0-9]+)?\s*$")
+REMOVE_REGEX = re.compile("´+[^>]+´+")
 DECIMALS = 3
 
 class UnitType:
 
     def __init__( self ):
         self._multiples = {}
-
+    
     def addMultiple( self, unit, multiple ):
         self._multiples[ multiple ] = unit;
         return self
-
+        
     def getStringFromMultiple(self, value, multiple):
-        numberString = str(round(value / multiple, DECIMALS));
+        value = round(value / multiple, DECIMALS);
+        if value == 0:
+            return
+        numberString = str(value);
         if numberString[-2:] == ".0":
             numberString = numberString[:-2]
         return numberString + self._multiples[multiple]
-
+    
     def getString( self, value ):
         sortedMultiples = sorted(self._multiples, reverse=True)
         for multiple in sortedMultiples:
@@ -45,100 +49,102 @@ class Unit:
         self._unitType = unitType
         self._toSIMultiplication = toSIMultiplication
         self._toSIAddition = toSIAddition
-
+    
     def toMetric( self, value ):
         return self._unitType.getString( ( value + self._toSIAddition ) * self._toSIMultiplication)
-
+    
     @abstractmethod
     def convert( self, message ): pass
 
 #NormalUnit class, that follow number + unit name.
 class NormalUnit( Unit ):
-    def __init__( self, regex, unitType, toSIMultiplication, toSIAddition = 0 ):
-        super( NormalUnit, self ).__init__(unitType, toSIMultiplication, toSIAddition)
-        self._regex = re.compile( regex + "(?![a-z])")
-
-    def convert( self, message ):
-        originalText = message.getText()
-        iterator = self._regex.finditer( originalText )
-        replacements = []
-        for find in iterator:
-            numberResult = END_NUMBER_REGEX.search( originalText[ 0 : find.start() ] )
-            if numberResult is not None:
-                repl = {}
-                repl[ "start" ] = numberResult.start()
-                repl[ "text"  ] = self.toMetric( float( numberResult.group() ) )
-                repl[ "end" ] = find.end()
-                replacements.append(repl)
-        if len(replacements)>0:
-            lastPoint = 0
-            finalMessage = ""
-            for repl in replacements:
-                finalMessage += originalText[ lastPoint: repl[ "start" ] ] + repl[ "text" ]
-                lastPoint = repl["end"]
-            finalMessage += originalText[ lastPoint : ]
-            message.setText(finalMessage)
+        def __init__( self, regex, unitType, toSIMultiplication, toSIAddition = 0 ):
+            super( NormalUnit, self ).__init__(unitType, toSIMultiplication, toSIAddition)
+            self._regex = re.compile( regex + "(?![a-z])")
+        
+        def convert( self, message ):
+            originalText = message.getText()
+            iterator = self._regex.finditer( originalText )
+            replacements = []
+            for find in iterator:
+                numberResult = END_NUMBER_REGEX.search( originalText[ 0 : find.start() ] )
+                if numberResult is not None:
+                    metricValue = self.toMetric( float( numberResult.group().replace(",", ".") ) ) 
+                    if metricValue is None:
+                        continue
+                    repl = {}
+                    repl[ "start" ] = numberResult.start()
+                    repl[ "text"  ] = metricValue
+                    repl[ "end" ] = find.end()
+                    replacements.append(repl)
+            if len(replacements)>0:
+                lastPoint = 0
+                finalMessage = ""
+                for repl in replacements:
+                    finalMessage += originalText[ lastPoint: repl[ "start" ] ] + repl[ "text" ]
+                    lastPoint = repl["end"]
+                finalMessage += originalText[ lastPoint : ]
+                message.setText(finalMessage)
 
 # Class containing a string, for the modificable message, and a boolean
 # to indicate if the message has been modified
 class ModificableMessage:
-
+    
     def __init__(self, text):
         self._text = text
         self._modified = False
-
+        
     def getText(self):
         return self._text
-
+    
     def setText(self, text):
         self._text = text
         self._modified = True
-
+        
     def isModified(self):
         return self._modified
-
+        
 units = []
 
 #Distance units
-units.append( NormalUnit("in(ch(es)?)?|\"|''", DISTANCE, 0.0254) )	#inch
-units.append( NormalUnit("f(oo|ee)?t|'", DISTANCE, 0.3048) )		#foot
-units.append( NormalUnit("mi(les?)?", DISTANCE, 1609.344) )			#mile
+units.append( NormalUnit("in(ch(es)?)?|\"|''", DISTANCE, 0.0254) )    #inch
+units.append( NormalUnit("f(oo|ee)?t|'", DISTANCE, 0.3048) )        #foot
+units.append( NormalUnit("mi(les?)?", DISTANCE, 1609.344) )            #mile
 
 #Area
-units.append( NormalUnit("in(ch(es)?)? ?(\^2|squared)", DISTANCE, 0.00064516) )	#inch squared
-units.append( NormalUnit("f(oo|ee)?t ?(\^2|squared)", DISTANCE, 0.092903) )		#foot squared
-units.append( NormalUnit("mi(les?)? ?(\^2|squared)", DISTANCE, 2589990) )		#mile squared
-units.append( NormalUnit("acres?", AREA, 4046.8564224 ) )						#acre
+units.append( NormalUnit("in(ch(es)?)? ?(\^2|squared)", DISTANCE, 0.00064516) )    #inch squared
+units.append( NormalUnit("f(oo|ee)?t ?(\^2|squared)", DISTANCE, 0.092903) )        #foot squared
+units.append( NormalUnit("mi(les?)? ?(\^2|squared)", DISTANCE, 2589990) )        #mile squared
+units.append( NormalUnit("acres?", AREA, 4046.8564224 ) )                        #acre
 #Volume
-units.append( NormalUnit( "pints?|pt|p", VOLUME, 0.473176 ) )	#pint
-units.append( NormalUnit( "quarts?|qt", VOLUME, 0.946353 ) )	#quart
-units.append( NormalUnit( "gal(lons?)?", VOLUME, 3.78541 ) )	#galon
+units.append( NormalUnit( "pints?|pt|p", VOLUME, 0.473176 ) )    #pint
+units.append( NormalUnit( "quarts?|qt", VOLUME, 0.946353 ) )    #quart
+units.append( NormalUnit( "gal(lons?)?", VOLUME, 3.78541 ) )    #galon
 
 #Energy
-units.append( NormalUnit("ft( |\*)?lbf?|foot( |-)pound", ENERGY, 1.355818) )	#foot-pound
+units.append( NormalUnit("ft( |\*)?lbf?|foot( |-)pound", ENERGY, 1.355818) )    #foot-pound
 
 #Force
-units.append( NormalUnit("pound( |-)?force|lbf", FORCE, 4.448222) )	#pound-force
+units.append( NormalUnit("pound( |-)?force|lbf", FORCE, 4.448222) )    #pound-force
 
 #Torque
-units.append( NormalUnit("Pound(-| )?foot|lbf( |\*)?ft", TORQUE, 1.355818) )	#pound-foot
+units.append( NormalUnit("Pound(-| )?foot|lbf( |\*)?ft", TORQUE, 1.355818) )    #pound-foot
 
 #Velocity
-units.append( NormalUnit("miles? per hour|mph", VELOCITY, 0.44704) )	#miles per hour
+units.append( NormalUnit("miles? per hour|mph", VELOCITY, 0.44704) )    #miles per hour
 
 #Mass
-units.append( NormalUnit( "ounces?|oz", MASS, 28.349523125 ) )	#ounces
-units.append( NormalUnit( "pounds?|lbs?", MASS, 453.59237 ) )	#pounds
-units.append( NormalUnit( "stones?|st?", MASS, 6350.2293318 ) )	#stones
+units.append( NormalUnit( "ounces?|oz", MASS, 28.349523125 ) )    #ounces
+units.append( NormalUnit( "pounds?|lbs?", MASS, 453.59237 ) )    #pounds
 
 #Temperature
-units.append( NormalUnit("º?F|(degrees ?)?farenheit", TEMPERATURE, 5/9, -32 ) )	#Degrees freedom
+units.append( NormalUnit("º?F", TEMPERATURE, 5/9, -32 ) )    #Degrees freedom
 #Pressure
 units.append( NormalUnit( "pounds?((-| )?force)? per square in(ch)?|lbf\/in\^2|psi", PRESSURE, 0.068046 ) ) #Pounds per square inch
-
+ 
 #Processes a string, converting freedom units to science units.
-def process(message):
-    modificableMessage = ModificableMessage(message)
+def process(message):    
+    modificableMessage = ModificableMessage(REMOVE_REGEX.sub("", message))
     for u in units:
         u.convert(modificableMessage)
     if modificableMessage.isModified():
